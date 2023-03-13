e104d3c685ad63e4319c930e9bb16f26f75a6d66
Mohanakhil <akhilmohan768@gmail.com>
2023-03-13 09:58:59 +0530

    dev:    lint:1    coverage:85% removeWireMockServer:    lint:1    coverage:85%

--- src/main/java/com/techsophy/tsf/commons/acl/ACLEvaluator.java
+++ src/main/java/com/techsophy/tsf/commons/acl/ACLEvaluator.java
@@ -1,25 +1,23 @@
 package com.techsophy.tsf.commons.acl;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
-
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-@RequiredArgsConstructor
 public class ACLEvaluator
 {
     private final String gatewayURL;
-    private final WebClient webClient;  //creates only single instance of webClient
+    private final WebClient webClient;
 
     public ACLEvaluator(String gatewayURL)
     {
         this.gatewayURL=gatewayURL;
-        this.webClient=WebClient.builder().build();
+        this.webClient=WebClient.builder().build();          //creates only single instance of webClient
     }
+
     private ACLValidate evaluateACL(Map<String,?> context,String aclId,String token)
     {
         var contextMap= Collections.singletonMap("context",new HashMap<>());
