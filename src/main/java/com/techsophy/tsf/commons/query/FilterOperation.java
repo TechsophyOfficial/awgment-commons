d76a0d48b894563ebf14658518eac6f9b23f4d66
nandini <nandini.k@techsophy.com>
2023-04-18 15:15:24 +0530

    Testcases added

--- src/main/java/com/techsophy/tsf/commons/query/FilterOperation.java
+++ src/main/java/com/techsophy/tsf/commons/query/FilterOperation.java
@@ -2,14 +2,13 @@ package com.techsophy.tsf.commons.query;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import org.springframework.data.mongodb.core.query.Criteria;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
 @JsonSubTypes({@JsonSubTypes.Type(value = EqualsOperation.class),
         @JsonSubTypes.Type(value = ComparatorOperation.class),
         @JsonSubTypes.Type(value = InOperation.class),
         @JsonSubTypes.Type(value = LikeOperation.class)})
-public interface FilterOperation<T> extends CriteriaTransformer<T>
+public interface FilterOperation extends CriteriaTransformer
 {
 
 }
