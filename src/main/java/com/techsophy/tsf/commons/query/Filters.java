b0022999b2341001c8a0df618b1365f3cb839b4a
Mohanakhil <akhilmohan768@gmail.com>
2023-04-26 14:57:45 +0530

    dev:   lint:0   coverage:85% utilityAddition:   lint:0   coverage:85%

--- src/main/java/com/techsophy/tsf/commons/query/Filters.java
+++ src/main/java/com/techsophy/tsf/commons/query/Filters.java
@@ -8,16 +8,12 @@ import java.util.stream.Collectors;
 @Data
 public class Filters
 {
-
-
     private Map<String, FilterOperation> operations;
 
-    <T> T buildAndQuery(QueryBuilder<T> builder){
+   public  <T> T buildAndQuery(QueryBuilder<T> builder){
         return builder.andQueries(
                  operations.entrySet().stream().map(
                         entry -> entry.getValue().getCriteria(entry.getKey(), builder)
                 ).filter(Objects::nonNull).collect(Collectors.toList()));
-
     }
-
 }
